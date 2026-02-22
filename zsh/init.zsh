3382fac2c43d3ba29c3eca9bd63fc610ef9a3910
kefhuang <aqr.kefhuang@gmail.com>
2026-02-22 16:42:11 +0800

    restructure zsh config: split into env, init, and prompt modules
    - Remove .common.zshrc, .p10k.zsh, plugins.zsh
    - Add env.zsh, init.zsh, prompt.zsh
    - Update zimrc and zshrc entrypoint
    - Remove wezterm config

---
+++ zsh/init.zsh
@@ -0,0 +1,48 @@
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+ZIM_HOME=~/.zim
+ZIM_CONFIG_FILE=~/.config/zsh/zimrc
+
+zstyle ':zim:git' aliases-prefix 'g'
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+fi
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE} ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init
+fi
+source ${ZIM_HOME}/init.zsh
+
+if [[ -d "$HOME/miniconda3" ]]; then
+  conda() {
+    unfunction conda
+    __conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+      eval "$__conda_setup"
+    else
+      if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+      fi
+    fi
+    unset __conda_setup
+    conda "$@"
+  }
+fi
+
+if [[ -d "$HOME/.nvm" ]]; then
+  export NVM_DIR="$HOME/.nvm"
+  _load_nvm() {
+    unfunction nvm node npm npx 2>/dev/null
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+  }
+  nvm() { _load_nvm && nvm "$@"; }
+  node() { _load_nvm && node "$@"; }
+  npm() { _load_nvm && npm "$@"; }
+  npx() { _load_nvm && npx "$@"; }
+fi
